2cc6f2a50661c764f4ce28680dcdf66135808860
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2024-11-22 17:19:14 -0300

    create models

---
+++ app/Models/ResourceStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResourceStatus extends Model
+{
+    use HasFactory;
+}
