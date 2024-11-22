99b585372a5b2bb3e35a0899bd5bc9a1a8552931
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2024-11-22 17:42:06 -0300

    fetch delivered tasks by date

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Task;
 
 class ReportController extends Controller
 {
-    //
+    public function __invoke($year, $month)
+    {
+
+        $tasks = Task::with(['resource', 'taskCounts', 'workOrderItemDetail.workOrderItem.workOrder'])
+            ->where('task_status_id', 4)
+            ->whereHas('resource')
+            ->whereHas('workOrderItemDetail.workOrderItem.workOrder', function ($query) use ($year, $month) {
+                $query->whereYear('created', $year)
+                      ->whereMonth('created', $month);
+            })
+            ->get();
+
+            //retornar un json con el array de datos
+            return response()->json($tasks);
+    }
 }
